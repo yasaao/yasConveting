fa76f3c5e4b58a9e7e56d3449e1d9cef4d8625cb
yasaao <143854211+yasaao@users.noreply.github.com>
2025-10-22 09:38:36 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ from PIL import Image
 # --------------------------
 app = Flask(__name__)
 # Ganti dengan kunci rahasia unik Anda
-app.config['yasabcdefghijklmno'] = 'KUNCI_RAHASIA_YANG_KUAT_DAN_UNIK_UNTUK_VERCEL' # <--- GANTI TEKS INI
+app.config['SECRET_KEY'] = 'yasabcdefghijklmno' # <--- GANTI TEKS INI
 app.config['PERMANENT_SESSION_LIFETIME'] = 3600
 
 # Storage In-Memory: Data file sementara yang diupload
